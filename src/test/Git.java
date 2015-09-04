62f32cd394d42d2ace3f74d8a89889a773feca80
Hector Porras <hector.porras.87@gmail.com>
2015-09-04 16:23:04 -0600

    Ramita creada

--- src/test/Git.java
+++ src/test/Git.java
@@ -3,6 +3,9 @@ package test;
 public class Git {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		int array[] = {69, 42, 71, 666};
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
 }
